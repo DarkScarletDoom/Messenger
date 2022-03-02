16374eb67aeed1832a992f20a80f689b1458b9c2
DarkScarletDoom <93650805+DarkScarletDoom@users.noreply.github.com>
2022-03-02 22:36:19 +0400

    ыыыы

--- js/index.js
+++ js/index.js
@@ -1,8 +1,28 @@
+function createMessege (text) {
+    newDiv = document.createElement("div")
+    document.querySelector('#content').insertBefore(newDiv, document.querySelector("#messegeInputDiv")).style.color = "rgb(233, 233, 233)"
+    newP = document.createElement("p")
+    newP.textContent = text;   
+    newDiv.appendChild(newP)
+
+    let now = new Date();
+    hours = now.getHours()
+    minutes = now.getMinutes()
+    time = hours + ":" + minutes
+    console.log(time) 
+
+    newP = document.createElement("p")
+    newP.textContent = time;   
+    newDiv.appendChild(newP)
+}
+
 document.getElementById("chatsNav").onclick = () => {
     chats = document.getElementById("chats")
     if(chats.style.display == 'none'){
         chats.style.display = 'block'
         document.getElementById("modal").style.display = 'none'
+        document.getElementById('content').style.backgroundImage = "none"
+        document.getElementById('messegeInputDiv').style.display = "none"
     }
 }
 
@@ -21,5 +41,23 @@ document.getElementById("searchUsersNav").onclick = () => {
                 }
             }
         }
+    }
+}
+
+chatsDiv = document.getElementById('chats')
+chatsDiv.onclick = (event) => {
+    if(chatsDiv.style.display != "none"){
+        chatsDiv.style.display = "none"
+        document.getElementById('content').style.backgroundImage = "url('img/atlantic-1794462_1280.jpg')"
+        document.getElementById('messegeInputDiv').style.display = "flex"
+
+        input = document.getElementById('messegeInput')
+        input.addEventListener('keypress', function(event) {
+            val = input.value.trim().toLowerCase()
+            if(event.code == 'Enter' && val != '' || event['which'] == 13){
+                input.value = ""
+                createMessege(val)
+            }
+        })
     }
 }
