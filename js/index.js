5ea221d5d1a3eddd8b90247513a262e5e9eb204f
Илья <93593987+Ilyaguar@users.noreply.github.com>
2022-03-02 21:12:38 +0400

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -49,3 +49,23 @@ document.getElementById("aboutProjectNav").onclick = () => {
         closeModal(modal.style.display)
     }
 }
+nightMode = document.getElementById("nightModeNav")
+rootStyle = document.documentElement.style
+nightMode.onclick = () => {
+    if(nightMode.classList.contains("light")){
+        nightMode.classList.add("dark")
+        nightMode.classList.remove("light")
+        rootStyle.setProperty('--current-bg-color', 'var(--dark-theme-bg)');
+        rootStyle.setProperty('--current-main-color', 'var(--dark-theme-main)');
+        rootStyle.setProperty('--bg-image-opacity', '0.45');
+        rootStyle.setProperty('--current-main-text-color', 'var(--dark-main-text-color)');
+    }
+    else {
+        nightMode.classList.add("light")
+        nightMode.classList.remove("dark")
+        rootStyle.setProperty('--current-bg-color', 'var(--light-theme-bg)');
+        rootStyle.setProperty('--current-main-color', 'var(--light-theme-main)');
+        rootStyle.setProperty('--bg-image-opacity', '0.6');
+        rootStyle.setProperty('--current-main-text-color', 'var(--light-main-text-color)');
+    }
+}
