4299c0491c74a5081ae1bfd2701645ca454e929c
DarkScarletDoom <93650805+DarkScarletDoom@users.noreply.github.com>
2022-03-12 14:11:38 +0400

    verstka

--- js/index.js
+++ js/index.js
@@ -16,6 +16,24 @@ function createMessege (text) {
     newDiv.appendChild(newP)
 }
 
+function closeModal(display) {
+    modal = document.getElementById("modal")
+    modalContent = document.getElementById("modalContent")
+    if(modal.style.display == "flex"){
+        modal.onclick = (event) => {
+            const withinBoundaries = event.composedPath().includes(modalContent) //получаем путь до объекта включая модальное окно
+            if (!withinBoundaries) {
+                if(modal.style.display == "flex"){
+                    for (var i = 0; i < modalContent.children.length; i++) {
+                        modalContent.children[i].style.display = "none"
+                        modal.style.display = "none"
+                    }
+                }
+            }
+        }
+    }
+}
+
 document.getElementById("chatsNav").onclick = () => {
     chats = document.getElementById("chats")
     if(chats.style.display == 'none'){
@@ -59,5 +77,44 @@ chatsDiv.onclick = (event) => {
                 createMessege(val)
             }
         })
+    }
+}
+
+document.getElementById("createChatNav").onclick = () => {
+    if(modal.style.display == "none"){
+        modal.style.display = "flex"
+        document.getElementById("createChat").style.display = "block"
+        closeModal(modal.style.display)
+    }
+}
+
+document.getElementById("aboutProjectNav").onclick = () => {
+    if(modal.style.display == "none"){
+        modal.style.display = "flex"
+        document.getElementById("aboutProject").style.display = "block"
+        closeModal(modal.style.display)
+    }
+}
+
+nightMode = document.getElementById("nightModeNav")
+rootStyle = document.documentElement.style
+nightMode.onclick = () => {
+    if(nightMode.classList.contains("light")){
+        nightMode.classList.add("dark")
+        nightMode.classList.remove("light")
+        rootStyle.setProperty('--current-bg-color', 'var(--dark-theme-bg)');
+        rootStyle.setProperty('--current-main-color', 'var(--dark-theme-main)');
+        rootStyle.setProperty('--bg-image-opacity', '0.6');
+        rootStyle.setProperty('--current-main-text-color', 'var(--dark-main-text-color)');
+        rootStyle.setProperty('--current-theme-hover', 'var(--dark-theme-hover)');
+    }
+    else {
+        nightMode.classList.add("light")
+        nightMode.classList.remove("dark")
+        rootStyle.setProperty('--current-bg-color', 'var(--light-theme-bg)');
+        rootStyle.setProperty('--current-main-color', 'var(--light-theme-main)');
+        rootStyle.setProperty('--bg-image-opacity', '1');
+        rootStyle.setProperty('--current-main-text-color', 'var(--light-main-text-color)');
+        rootStyle.setProperty('--current-theme-hover', 'var(--light-theme-hover)');
     }
 }
